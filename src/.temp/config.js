a8debde173b3ea61521a303856e1c90f5ef64003
seojihyuk26 <seojihyuk26@gmail.com>
2022-03-29 11:25:00 +0900

    add gitignore

---
+++ src/.temp/config.js
@@ -0,0 +1,8 @@
+export default {
+  "trailingSlash": true,
+  "pathPrefix": "",
+  "titleTemplate": "%s - Seo Jihyuk blog",
+  "siteUrl": "https://seojihyuk26.github.io",
+  "version": "0.7.23",
+  "catchLinks": true
+}
---
+++ src/.temp/now.js
@@ -0,0 +1,1 @@
+export default 1648487697835
